5733ecfc4ea5c7cf06e5d7f08624afdcb32a60e0
Brian J Hoskins <brian@hoskins.eu>
2017-05-18 15:47:02 +0100

    re-write of project using LINUX as the development platform

---
+++ OLTRA/OLTRA/AboutDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OLTRA
+{
+    public partial class AboutDialog : Gtk.Dialog
+    {
+        public AboutDialog()
+        {
+            this.Build();
+        }
+    }
+}
+
---
+++ OLTRA/OLTRA/Logger.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace OLTRA
+{
+    #region LOGGER BASE
+    abstract class LoggerBase
+    {
+        /*
+        * TITLE:       [TITLE]
+        * DESCRIPTION: [DESCRIPTION]
+        * AUTHOR:      [NAME]
+        * DATE:        [DATE]
+        * VERSION:     [VERSION]
+        */
+
+        #region FIELDS
+        /* CONSTANT FIELDS */
+        /* FIELDS */
+        #endregion
+        #region CONSTRUCTORS
+        public LoggerBase()
+        {
+        }
+        #endregion
+        #region FINALIZERS
+        /* FINALIZERS */
+        #endregion
+        #region DELEGATES
+        /* DELEGATES */
+        #endregion
+        #region EVENTS
+        /* EVENTS */
+        #endregion
+        #region ENUMS
+        /* ENUMS */
+        #endregion
+        #region INTERFACES
+        /* INTERFACES */
+        #endregion
+        #region PROPERTIES
+        /* PROPERTIES */
+
+        #endregion
+        #region INDEXERS
+        /* INDEXERS */
+        #endregion
+        #region METHODS
+        /* METHODS */
+        public abstract int Run();
+        #endregion
+        #region STRUCTS
+        /* STRUCTS */
+        #endregion
+        #region CLASSES
+        /* CLASSES */
+        #endregion
+    }
+    #endregion          
+    #region TRW TYPE C LOGGER
+
+    #endregion
+    #region TRW TYPE A LOGGER
+    class TRW_TYPE_A : LoggerBase
+    {
+        /*
+        * TITLE:       [TITLE]
+        * DESCRIPTION: [DESCRIPTION]
+        * AUTHOR:      [NAME]
+        * DATE:        [DATE]
+        * VERSION:     [VERSION]
+        */
+
+        #region FIELDS
+        /* CONSTANT FIELDS */
+        /* FIELDS */
+        #endregion
+        #region CONSTRUCTORS
+        public TRW_TYPE_A()
+        {
+        }
+        #endregion
+        #region FINALIZERS
+        /* FINALIZERS */
+        #endregion
+        #region DELEGATES
+        /* DELEGATES */
+        #endregion
+        #region EVENTS
+        /* EVENTS */
+        #endregion
+        #region ENUMS
+        /* ENUMS */
+        #endregion
+        #region INTERFACES
+        /* INTERFACES */
+        #endregion
+        #region PROPERTIES
+        /* PROPERTIES */
+
+        #endregion
+        #region INDEXERS
+        /* INDEXERS */
+        #endregion
+        #region METHODS
+        /* METHODS */
+        public override int Run()
+        {
+            //SqliteConnection
+            return 0;
+        }
+        #endregion
+        #region STRUCTS
+        /* STRUCTS */
+        #endregion
+        #region CLASSES
+        /* CLASSES */
+        #endregion
+    }
+    #endregion
+}
+
