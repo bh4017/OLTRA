390c243f4f1edb4390e05e7959a12d26e900ca97
Brian J Hoskins <brian@hoskins.eu>
2017-06-25 21:37:21 +0100

    mostly work on the GUI

---
+++ OLTRA/Main.cs
@@ -0,0 +1,55 @@
+﻿namespace OLTRA
+{
+    using Gtk;
+    using System;
+
+    public class MainWindow
+    {
+        #region CONSTANT FIELDS
+        #endregion
+        #region FIELDS
+        #endregion
+        #region CONSTRUCTORS
+        public MainWindow()
+        {
+            Gtk.Application.Init();
+            Builder Gui = new Builder();
+            Gui.AddFromFile("../../Resources/MainWindow.glade");
+
+            Gui.Autoconnect(this);
+            Gtk.Application.Run();
+        }
+        #endregion
+        #region DESTRUCTORS
+        #endregion
+        #region DELEGATES
+        #endregion
+        #region EVENTS
+        #endregion
+        #region ENUMS
+        #endregion
+        #region INTERFACES
+        #endregion
+        #region PROPRERTIES
+        #endregion
+        #region INDEXERS
+        #endregion
+        #region METHODS
+        public static void Main()
+        {
+            new MainWindow();
+        }
+        #region EVENT HANDLERS
+        static void onDeleteEvent(object o, DeleteEventArgs args)
+        {
+            Application.Quit();
+        }
+        #endregion
+        #endregion
+        #region STRUCTS
+        #endregion
+        #region CLASSES
+        #endregion           
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 OLTRA
 One Logger To Rule (them) All
+
+STYLECOP
+I decided to try my best to conform to the coding rules laid out by stylecop.  See stylecop.soyuz5.com.tar.gz.  The following deliberate exceptions apply:
+1. I added a region inside the METHODS section to separate EVENT HANDLERS from normal Methods.
